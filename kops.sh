65b317ab013f369e885e7e322443ff6ba8294103
phani-adventure <145846174+phani-adventure@users.noreply.github.com>
2025-09-18 16:33:55 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -6,7 +6,7 @@
 #! /bin/bash
 aws configure
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-wget https://github.com/kubernetes/kops/releases/download/v1.33.0/kops-linux-amd64
+wget https://github.com/kubernetes/kops/releases/download/v1.33.1/kops-linux-amd64
 chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
