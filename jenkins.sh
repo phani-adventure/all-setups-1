147a3ddbc9406aafadd30a5178b4df18ae51ba49
phani-adventure <145846174+phani-adventure@users.noreply.github.com>
2026-05-23 17:32:32 +0530

    Modify jenkins.sh for Java 21 and cleanup
    Updated Jenkins installation script to use Java 21 and removed unnecessary commands.

--- jenkins.sh
+++ jenkins.sh
@@ -1,16 +1,11 @@
-sudo yum update –y
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+
+#! /bin/bash
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade
-sudo yum install java-17-amazon-corretto -y
-sudo yum install jenkins git -y
-sudo systemctl enable jenkins
-sudo systemctl start jenkins
-sudo systemctl status jenkins
-sudo mkdir -p /var/tmp_disk
-sudo chmod 1777 /var/tmp_disk
-sudo mount --bind /var/tmp_disk /tmp
-echo '/var/tmp_disk /tmp none bind 0 0' | sudo tee -a /etc/fstab
-sudo systemctl mask tmp.mount
-df -h /tmp
+# Add required dependencies for the jenkins package
+sudo yum install java-21-amazon-corretto
+sudo yum install jenkins -y
 sudo systemctl restart jenkins
+sudo systemctl daemon-reload
